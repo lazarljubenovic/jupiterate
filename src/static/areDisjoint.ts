26446cd0283f1d6e1480954de3a80ae16252b89c
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2019-11-17 00:45:04 +0100

    fix

--- src/static/areDisjoint.ts
+++ src/static/areDisjoint.ts
@@ -1,6 +1,6 @@
-import { union } from './union'
 import { size } from './size'
+import { intersection } from './intersection'
 
-export function areDisjoint(...iterables: Array<Iterable<unknown>>): boolean {
-  return size(union(...iterables)) == 0
+export function areDisjoint (...iterables: Array<Iterable<unknown>>): boolean {
+  return size(intersection(...iterables)) == 0
 }
